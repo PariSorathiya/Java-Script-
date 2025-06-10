e2edd8b0ab36edcf13bcc31eb752e14a2ff3a6fc
PariSorathiya <parisorathiya22@gmail.com>
2025-06-10 09:43:49 +0530

    Class Work

---
+++ Class Work/Data Type/script.js
@@ -0,0 +1,38 @@
+// let str = "don't use this";
+// let num = 23;
+// let bool = true
+// let nullD = null
+// // undefiend 
+// // console.log(b);
+// // bigint(big intezer)
+
+// let sym = symbol("sym")
+
+// console.log(typeof(str));
+// console.log(typeof(num));
+// console.log(typeof(bool));
+// console.log(typeof(nullD));
+// console.log(typeof(sym));
+
+
+let a = prompt("enter a number"); //String
+let b = confirm("Confirm this or not") // Boolean
+alert(typeof(b))
+
+
+let Birthyear = prompt("Enter your birthyear")
+alert(2025-Birthyear)
+
+// Implicit type conversion
+// Explicit type conversion
+
+
+// Template literals
+// `` backtick
+
+
+let naam = "Pari"
+
+let str = `my name is ${naam}`
+
+console.log(str);
---
+++ Class Work/Extra/script.js
@@ -0,0 +1,51 @@
+// console.log("Hello JS")
+// console.warn("This is a Warning")
+// console.error("This ia a error")
+
+// alert("This is an alert")
+// confirm("This is a confirmation box")
+// prompt("Enter your name :")
+
+// document.write("")
+
+// Variable => A container to store some value.
+
+// // In JS there are three kyeword used to createn variable->
+
+// // var, let, Const
+
+// var a = 10;
+// var a = 20, //re-declare, re-initialize
+// var b = 20;
+
+// let c = 35;
+// // let c = 50; re-declaration is not possible
+// c = 45; // re-intialization is possible
+
+// const d = 45; // re-declration and re- intialization both are not possible
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+
+// // declaration ----- initialization
+
+
+
+
+let a = 30;
+let b = 20;
+console.log(a-b);
+
+
+// let num1 = prompt("enter num 1");
+// let num2 = prompt("enter num 2");
+
+// alert(num1*num2)
+
+let text3 = prompt("enter your first name");
+let text4 = prompt("enter your last name");
+
+alert(text3+text4)
